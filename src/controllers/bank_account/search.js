efb90c3647fc2b3f1d35c18a22de843077d46885
Thamy <thamiris_oliveira2@hotmail.com>
2023-11-28 22:18:57 -0300

    Add rutes search and update

--- src/controllers/bank_account/search.js
+++ src/controllers/bank_account/search.js
@@ -1,13 +1,21 @@
+const knex = require('../../conexao')
 
 
 const searchAccount = async (req, res) => {
 
-    
-        
-    return res.status(400).json({mensagem: "Password incorret"})
+    const {bank_password} = req.query
+
+    const {password}= await knex('bank').first();
+
        
-    
-   
+    if(password !== bank_password){
+        return res.status(400).json({mensagem: "Password incorrect"}) 
+    }
+
+    const fundAccounts = await knex('accounts') 
+
+      
+    return res.status(200).json(fundAccounts)
 }
 
 module.exports = searchAccount
